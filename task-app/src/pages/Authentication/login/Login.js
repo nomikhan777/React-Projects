0c2914db97ba49a98f50a230710eb566d1b78512
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-05-11 11:01:55 +0500

    new commit

--- task-app/src/pages/Authentication/login/Login.js
+++ task-app/src/pages/Authentication/login/Login.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, {useState}from "react";
 
 export default function Login() {
+  const initialState = {email:"", password: ""}
+
+  const {state, seState} = useState(initialState);
+
+  const handleChange = e =>{
+    seState(s => ({...s,[e.target.name]:e.target.value}))
+  }
+  const handleLogin = (e) =>{
+    e.preventDefault()
+    let {email, password} = state
+
+    console.log(email);
+    console.log(password);
+
+
+
+  }
   return (
-    <div>
-        Login
-      
+    <div className="auth">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+            <div className="card py-2 py-md-3 py-lg-5">
+              <div className="row">
+                <div className="col">
+                  <h2>LOGIN</h2>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <input type="email" className="form-control" placeholder="Email" name="email" onChange={handleChange} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
