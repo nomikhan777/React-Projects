1d78e7c07f353c9c65ec1913b889fe470010bc4c
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-05-15 16:12:23 +0500

    new changes

--- task-app/src/components/PrivateRoute.js
+++ task-app/src/components/PrivateRoute.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import Login from '../pages/Authentication/login/Login'
 
 export default function PrivateRoute(props) {
 
+    const {authentication} = useContext(AuthContext)
+    const{isAuthenticated} = authentication
+
     const {Component} = props
+
+    if (!isAuthenticated)
+    return <Login/>
   return (
-    <div>Private Route </div>
+    <Component/>
   )
 }
--- task-app/src/pages/Routes.js
+++ task-app/src/pages/Routes.js
@@ -1,15 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import React from 'react'
+import React, { Component, useContext } from 'react'
 import Frontend from "./Frontend"
 import Authentication from "./Authentication"
 import Dashboard from "./Dashboard"
-export default function index() {
+import { AuthContext } from "../context/AuthContext";
+import PrivateRoute from "../components/PrivateRoute";
+export default function Index() {
+
+  const{ authentication } = useContext(AuthContext)
+  const{ isAuthenticated}  = authentication
+   console.log(isAuthenticated)
+
   return (
     <BrowserRouter>
     <Routes>
         <Route path="/*" element ={<Frontend/>}/>
-        <Route path="/authentication/*" element ={<Authentication/>}/>
-        <Route path="/dashboard/*" element ={<Dashboard/>}/>
+        <Route path="/authentication/*" element ={!isAuthenticated ? <Authentication/> : <Navigate to = "/dashboard"/>}/>
+        <Route path="/dashboard/*" element ={<PrivateRoute Component={Dashboard}/>} />
 
     </Routes>
       
